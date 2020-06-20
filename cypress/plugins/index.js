e66e5e9cc60a11f31460596f25eb5b5e2ce902b7
Lukas Lewandowski <llewandowski1990@gmail.com>
2020-06-20 16:57:31 +0200

    fix script for eslint + remove example tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,15 +18,15 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  on('file:preprocessor', cucumber());
-  on('task', {
-    log(message) {
-      console.log(message);
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    on('file:preprocessor', cucumber());
+    on('task', {
+        log(message) {
+            console.log(message);
 
-      return null;
-    },
-  });
-  return config;
+            return null;
+        },
+    });
+    return config;
 };
